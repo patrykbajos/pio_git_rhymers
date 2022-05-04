69d10d60f0e6de681de9e965f81223e4bb70a2e6
Patryk Klimas <patrykbajos@protonmail.ch>
2022-05-04 23:08:32 +0200

    15. Dodanie testow jednostkowych dla klas DefaultRhymersFactory, IntLinkedList, FIFORhymer, HanoiRhymer

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.factory.RhymersFactory;
+import edu.kis.vh.nursery.list.IntLinkedList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,6 +12,9 @@ public class RhymersJUnitTest {
 
     private static final int STACK_CAPACITY = 12;
     private static final int EMPTY_STACK_VALUE = -1;
+
+    public static final int ILL_VALUE_1 = 21;
+    public static final int ILL_VALUE_2 = 37;
 
     @Test
     public void testCountIn() {
@@ -76,5 +82,54 @@ public class RhymersJUnitTest {
     }
 
     // 14. wszystkie testy wykonuja sie poprawnie, brak zmian
+
+    @Test
+    public void testDefaultRhymersFactory() {
+        RhymersFactory rhymersFactory = new DefaultRhymersFactory();
+
+        Assert.assertNotNull(rhymersFactory.getStandardRhymer());
+        Assert.assertNotNull(rhymersFactory.getFalseRhymer());
+        Assert.assertNotNull(rhymersFactory.getFIFORhymer());
+        Assert.assertNotNull(rhymersFactory.getHanoiRhymer());
+    }
+
+    @Test
+    public void testIntLinkedList() {
+        IntLinkedList list = new IntLinkedList();
+        Assert.assertTrue(list.isEmpty());
+
+        list.push(ILL_VALUE_1);
+        Assert.assertEquals(list.top(), ILL_VALUE_1);
+        Assert.assertFalse(list.isEmpty());
+
+        list.push(ILL_VALUE_2);
+        Assert.assertEquals(list.pop(), ILL_VALUE_2);
+        Assert.assertEquals(list.pop(), ILL_VALUE_1);
+
+        Assert.assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testFIFORhymer() {
+        FIFORhymer rhymer = new FIFORhymer();
+
+        rhymer.countIn(1);
+        rhymer.countIn(2);
+        rhymer.countIn(3);
+
+        Assert.assertEquals(rhymer.countOut(), 1);
+        Assert.assertEquals(rhymer.countOut(), 2);
+        Assert.assertEquals(rhymer.countOut(), 3);
+
+        Assert.assertTrue(rhymer.callCheck());
+    }
+
+    @Test
+    public void testHanoiRhymer() {
+        HanoiRhymer rhymer = new HanoiRhymer();
+
+        rhymer.countIn(TEST_VALUE);
+        Assert.assertEquals(TEST_VALUE, rhymer.peekaboo());
+    }
 }
 // alt + strzałka zmienia karty w edytorze
